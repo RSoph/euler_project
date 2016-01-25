c2a9c18ed851eb8ff11627a70a7975cdff28383e
RSoph <kate.brender@gmail.com>
2016-01-25 17:34:59 -0500

    Problem 11 solved

--- 011_largest_product_in_a_grid.rb
+++ 011_largest_product_in_a_grid.rb
@@ -1,98 +1,113 @@
-require 'pry'
-
-class GridProduct
-
-	attr_accessor :product
-
-	def initialize(a, b, c, d) #e, f, g, h, i, j, k, l, m)
-		@a = a
-		@b = b
-		@c = c
-		@d = d
-		# @e = e
-		# @f = f
-		# @g = g
-		# @h = h
-		# @i = i
-		# @j = j
-		# @k = k
-		# @l = l
-		# @m = m
-	end
-
-	def product
-		product = (@a*@b*@c*@d) #, (@a*@e*@f*@g), (@a*@h*@i*@j), (@a*@k*@l*@m)].sort.last
-	end
-
-
-end
-
 grid = [
-[8, 2, 22, 97, 38, 15, 0, 40, 0, 75, 4, 5, 7, 78, 52, 12, 50, 77, 91, 8],
-[49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 4, 56, 62, 0],
-[81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30, 3, 49, 13, 36, 65],
-[52, 70, 95, 23, 4, 60, 11, 42, 69, 24, 68, 56, 1, 32, 56, 71, 37, 2, 36, 91],
-[22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 40, 40, 28, 66, 33, 13, 80],
-[24, 47, 32, 60, 99, 3, 45, 2, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12, 50],
-[32, 98, 81, 28, 64, 23, 67, 10, 26, 38, 40, 67, 59, 54, 70, 66, 18, 38, 64, 70],
-[67, 26, 20, 68, 2, 62, 12, 20, 95, 63, 94, 39, 63, 8, 40, 91, 66, 49, 94, 21],
-[24, 55, 58, 5, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72],
-[21, 36, 23, 9, 75, 0, 76, 44, 20, 45, 35, 14, 0, 61, 33, 97, 34, 31, 33, 95],
-[78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 3, 80, 4, 62, 16, 14, 9, 53, 56, 92],
-[16, 39, 5, 42, 96, 35, 31, 47, 55, 58, 88, 24, 0, 17, 54, 24, 36, 29, 85, 57],
-[86, 56, 0, 48, 35, 71, 89, 7, 5, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58],
-[19, 80, 81, 68, 5, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77, 4, 89, 55, 40],
-[4, 52, 8, 83, 97, 35, 99, 16, 7, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66],
-[88, 36, 68, 87, 57, 62, 20, 72, 3, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69],
-[4, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18, 8, 46, 29, 32, 40, 62, 76, 36],
-[20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74, 4, 36, 16],
-[20, 73, 35, 29, 78, 31, 90, 1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 5, 54],
-[1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
+	[8, 2, 22, 97, 38, 15, 0, 40, 0, 75, 4, 5, 7, 78, 52, 12, 50, 77, 91, 8], 
+	[49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 4, 56, 62, 0], 
+	[81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30, 3, 49, 13, 36, 65], 
+	[52, 70, 95, 23, 4, 60, 11, 42, 69, 24, 68, 56, 1, 32, 56, 71, 37, 2, 36, 91], 
+	[22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 40, 40, 28, 66, 33, 13, 80], 
+	[24, 47, 32, 60, 99, 3, 45, 2, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12, 50], 
+	[32, 98, 81, 28, 64, 23, 67, 10, 26, 38, 40, 67, 59, 54, 70, 66, 18, 38, 64, 70], 
+	[67, 26, 20, 68, 2, 62, 12, 20, 95, 63, 94, 39, 63, 8, 40, 91, 66, 49, 94, 21], 
+	[24, 55, 58, 5, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72], 
+	[21, 36, 23, 9, 75, 0, 76, 44, 20, 45, 35, 14, 0, 61, 33, 97, 34, 31, 33, 95], 
+	[78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 3, 80, 4, 62, 16, 14, 9, 53, 56, 92], 
+	[16, 39, 5, 42, 96, 35, 31, 47, 55, 58, 88, 24, 0, 17, 54, 24, 36, 29, 85, 57], 
+	[86, 56, 0, 48, 35, 71, 89, 7, 5, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58], 
+	[19, 80, 81, 68, 5, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77, 4, 89, 55, 40], 
+	[4, 52, 8, 83, 97, 35, 99, 16, 7, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66], 
+	[88, 36, 68, 87, 57, 62, 20, 72, 3, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69], 
+	[4, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18, 8, 46, 29, 32, 40, 62, 76, 36], 
+	[20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74, 4, 36, 16], 
+	[20, 73, 35, 29, 78, 31, 90, 1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 5, 54], 
+	[1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
 ]
 
 largest_product = 1
-x = 0
-y = 0
 
-while x < grid[0].size - 4
-	while y < grid.size - 4
-		current = GridProduct.new(grid[x][y], grid[x + 1][y], grid[x+2][y], grid[x+3][y]) # to the right
-		if current.product > largest_product
-			largest_product = current.product
+x = 20
+y = 20
+
+# First left to right, as long as x coordinate is less than 17
+a = 0
+while a < (x-3)
+	b = 0
+	while b < (y)
+		product = grid[a][b] * grid[a+1][b] * grid[a+2][b] * grid[a+3][b]
+		if product > largest_product
+			largest_product = product
 		end
-		current = GridProduct.new(grid[x][y], grid[x][y+1], grid[x][y+2], grid[x][y+3]) # down
-		if current.product > largest_product
-			largest_product = current.product
-		end
-		current = GridProduct.new(grid[x][y], grid[x+1][y+1], grid[x+2][y+2], grid[x+3][y+3]) # down and to the right
-		if current.product > largest_product
-			largest_product = current.product
-		end
-		y += 1
+		b+=1
 	end
-	x += 1
+	a+=1
 end
 
-x = 3
-y = 3
-
-while x < grid[0].size - 4
-	while y < grid.size - 4
-		current = GridProduct.new(grid[x][y], grid[x-1][y+1], grid[x-2][y+2], grid[x-3][y+3]) # down and to the left
-		if current.product > largest_product
-			largest_product = current.product
+# Right to left, but only when x coordinate is greater than 17
+a = 16
+while a < 19
+	b = 0
+	while b < (y)
+		product = grid[a-3][b] * grid[a-2][b] * grid[a-1][b] * grid[a][b]
+		if product > largest_product
+			largest_product = product
 		end
-		y += 1
+		b+=1
 	end
-	x += 1
+	a+=1
+end
+
+# Up to down, as long as y coordinate is less than 17
+a = 0
+while a < x
+	b = 0
+	while b < 17
+		product = grid[a][b] * grid[a][b+1] * grid[a][b+2] * grid[a][b+3]
+		if product > largest_product
+			largest_product = product
+		end
+		b+=1
+	end
+	a+=1
+end
+
+# Down to up, but only when y coordinate is greater than 17
+a = 0
+while a < x
+	b = 16
+	while b < 19
+		product = grid[a][b] * grid[a][b-1] * grid[a][b-2] * grid[a][b-3]
+		if product > largest_product
+			largest_product = product
+		end
+		b+=1
+	end
+	a+=1
+end
+
+# Diagional from up left to down right
+a = 0
+while a < 17
+	b = 0
+	while b < 17
+		product = grid[a][b] * grid[a+1][b+1] * grid[a+2][b+2] * grid[a+3][b+3]
+		if product > largest_product
+			largest_product = product
+		end
+		b+=1
+	end
+	a+=1
+end
+
+# Diagional from up right to down left
+a = 3
+while a < 20
+	b = 0
+	while b < 17
+		product = grid[a][b] * grid[a-1][b+1] * grid[a-2][b+2] * grid[a-3][b+3]
+		if product > largest_product
+			largest_product = product
+		end
+		b+=1
+	end
+	a+=1
 end
 
 puts largest_product
-
-
-
-
-
-
-
-
