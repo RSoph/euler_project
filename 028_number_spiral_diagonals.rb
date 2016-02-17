998c7575f68799d71e50b004810b06fcbf7f7e1b
RSoph <kate.brender@gmail.com>
2016-02-17 14:03:15 -0500

    refactored 28 and solved 22

--- 028_number_spiral_diagonals.rb
+++ 028_number_spiral_diagonals.rb
@@ -2,12 +2,8 @@ def sum_diags
 	total = 1
 	square = 3
 	while square < 1002
-		newitem = square * square
-		total += newitem
-		3.times do
-			newitem = newitem - square + 1
-			total += newitem			
-		end
+		fourcorners = 4*(square*square) - 6*(square) + 6
+		total += fourcorners		
 		square += 2
 	end
 	total
