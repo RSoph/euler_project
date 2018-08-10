286c0e74fa8efb9dc198b900b33ddb58cc0dc740
RSoph <katebrender@gmail.com>
2018-08-10 12:22:16 -0400

    code cleanup

--- 004_largest_palindrome_product.rb
+++ 004_largest_palindrome_product.rb
@@ -1,19 +1,23 @@
+# Find the largest palindrome made from the product of two 3-digit numbers.
+
 counter1 = 999
-palindromes = []
+max_palindrome = 99
 
 while counter1 > 99
 	counter2 = 999
 	while counter2 > 99
-		if (counter1*counter2).to_s == (counter1*counter2).to_s.reverse
-			if palindromes.sort[-1] && counter1*counter2 < palindromes.sort[-1]
-				break
-			else
-				palindromes << counter1*counter2
-			end
+		if (
+			(counter1*counter2).to_s == (counter1*counter2).to_s.reverse and
+			counter1*counter2 > max_palindrome
+			)
+				max_palindrome = counter1*counter2
 		end
 		counter2 -= 1
 	end
 	counter1 -= 1
 end
 
-puts palindromes.sort[-1]
+puts max_palindrome
+
+# My first solution involved keeping a list of all the palindromes I encountered, sorting them all the time,
+# and finally returning the last one. That was silly, now I'm only keeping track of the largest.
