ee507917f14930dda3e1d8d72c67162c2bcf39ce
RSoph <katebrender@gmail.com>
2018-08-10 12:54:28 -0400

    Problem 5 was unreadable, cleaned it up

--- 005_smallest_multiple.rb
+++ 005_smallest_multiple.rb
@@ -1,4 +1,7 @@
-def multiple(num)
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+# Finds the factorial of a number
+def factorial(num)
 	product = 1
 	while num > 1
 		product = product*num
@@ -7,6 +10,7 @@ def multiple(num)
 	product
 end
 
+# Verifies that product is a common multiple of every number up to num
 def check_number(num, product)
 	result = true
 	(1..num).each do |number|
@@ -18,10 +22,11 @@ def check_number(num, product)
 	return result
 end
 
-def find_primes(num)
+# Returns a list of primes under num
+def find_primes_under(num)
 	primes = [2]
 	counter = 3
-	while counter <= num
+	while primes[-1] < num
 		counter2 = 2
 		while counter2 <= counter
 			if counter % counter2 == 0
@@ -35,10 +40,13 @@ def find_primes(num)
 		end
 	counter += 1
 	end
-	primes
+	primes[0..-2]
 end
 
-def find_smallest(num, product, primes, check_number)
+# Returns the smallest common multiple of all numbers 1-num
+def find_smallest_common_multiple(num)
+	primes = find_primes_under(num)
+	product = factorial(num)
 	counter = 0
 	while counter < primes.size
 		while check_number(num, product)
@@ -50,4 +58,15 @@ def find_smallest(num, product, primes, check_number)
 	product
 end
 
-puts find_smallest(20, multiple(20), find_primes(20), check_number(20, multiple(20)))			
+puts find_smallest_common_multiple(20)
+
+# TL;DR:
+
+# 1) Multiply up all the numbers 1-20 (call it PRODUCT, it's on line 49).
+# PRODUCT is, by definition a common multiple of those numbers.
+
+# 2) Iterate through all the primes under 20, try dividing PRODUCT by that prime and checking if it's
+# still a common multiple. If so, go with that, if not, multiply that prime back in. This is lines
+# 51-57.
+
+# By the time you've gone through all those primes, you'll have the smallest reminaing multiple of 1-20.
