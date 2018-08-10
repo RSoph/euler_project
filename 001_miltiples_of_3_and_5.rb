8934bb42bd3f0e035c8ed6c0b099818b5148b505
RSoph <katebrender@gmail.com>
2018-08-10 12:05:24 -0400

    code cleanup - I used to be way dumber

--- 001_miltiples_of_3_and_5.rb
+++ 001_miltiples_of_3_and_5.rb
@@ -1,7 +1,8 @@
-counter = 1
-sum = 0
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+# Find the sum of all the multiples of 3 or 5 below 1000.
 
-while counter < 1000
+sum = 0
+(1..999).each do |counter|
 	if counter % 3 == 0
 		sum += counter
 	elsif counter % 5 == 0
@@ -11,4 +12,3 @@ while counter < 1000
 end
 
 puts sum
-puts "kate"
--- 002_even_fibinacci_numbers.rb
+++ 002_even_fibinacci_numbers.rb
@@ -1,8 +1,9 @@
+# By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
+
 sum = 0
 
 x = 1
 y = 1
-z = 2
 
 while x < 4000000
 	if x % 2 == 0
@@ -14,3 +15,8 @@ while x < 4000000
 end
 
 puts sum
+
+# Another solution would have been creating an array of numbers, and then adding up every other one.
+# I like this better because a) you're already iterating over the fibonacci numbers under 4 million,
+# you'd have to iterate over them again to get the sum and b) you'd have to hold all those numbers
+# in memory even though you only care about half of them, and you only care about those momentarily.
