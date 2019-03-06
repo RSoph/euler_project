9c6bb6a86bb42f70883b6b5dc71cd0ec998f8745
RSoph <katebrender@gmail.com>
2019-03-06 16:20:27 -0500

    problem 26

--- 026_reciprocal_cycles.rb
+++ 026_reciprocal_cycles.rb
@@ -4,12 +4,18 @@
 longest_cycle = {:d => 1, :length => 1}
 d = 1
 
-def cycle_length(num)
-	return 4
+def cycle_length(n, d, array=[])
+	if array.include?(n)
+		array.size - array.index(n)
+	else
+		new_n = 10 * (n - (n / d) * d)
+		array << n
+		cycle_length(new_n, d, array)
+	end
 end
 
 while d < 1000
-	length = cycle_length(d)
+	length = cycle_length(1, d)
 	if length > longest_cycle[:length]
 		longest_cycle = {:d => d, :length => length}
 	end
@@ -19,6 +25,6 @@ end
 puts longest_cycle
 
 # I wrote this absent-mindedly while trying to figure out how the method cycle_length
-# should work. I'm still not sure. You could do something like long division, where you keep
+# should work. I'm still not sure. I'm picturing something like long division, where you keep
 # adding a 0 and finding the remainder. When you finally hit the same remainder again,
-# you've completed one cycle. One day I'll try that out.
+# you've completed one cycle. One day I'll try to make it more graceful.
