5e1ee5f4925f989af483d70d7144dbc93d670a98
Kate Brender <kate.brender@gmail.com>
2020-08-25 00:45:05 -0400

    Benchmarked problem 24

--- 024_lexicographic_permutations.rb
+++ 024_lexicographic_permutations.rb
@@ -9,6 +9,9 @@
 # What is the millionth lexicographic permutation of the digits 
 # 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
 
+require 'Benchmark'
+
+puts Benchmark.measure { [0,1,2,3,4,5,6,7,8,9].permutation.to_a[999999].join() }
 puts [0,1,2,3,4,5,6,7,8,9].permutation.to_a[999999].join()
 
 # This gives an array with elements already in lexicographic order, because I provided the
